3a3a270e39434d425eb2e6575b19993fdf45dc82
Fabian <faalgoro@gmail.com>
2022-04-24 19:11:58 -0600

    Lab 1 web

---
+++ my-project/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return(
+    <body>
+      <h1 className='text-3xl fond-bold'>Fabian Alonso Gonzalez Rojas B83493</h1>
+    </body>
+  )
+}
+
+export default App;
