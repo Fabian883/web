851c510ee9507a7243ad34a4760d95d755336083
Fabian Alonso Gonzalez Rojas <b83493@ecci.ucr.ac.cr>
2022-04-28 14:36:35 -0600

    New folder for the components of the app

---
+++ my-project/src/Component/Header/index.js
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <div className = "flex items-center justify-center bg-purple-600 text-white h-16 w-full">
+            <p> ¡Solo por hoy 10% de descuento en todos los amigurumis rosados!</p>
+        </div>
+    );
+}
+
+export default Header;
--- my-project/src/Pages/App/App.js
+++ my-project/src/Pages/App/App.js
@@ -9,6 +9,8 @@ function App() {
       <h1 className='text-3xl fond-bold'>Fabian Alonso Gonzalez Rojas B83493</h1>
     </body>
     */
+    
+    /* Add here new paths if needed */
     <BrowserRouter>
       <Routes>
         <Route path="/" element= {<Home />} />
