5db3846cdfb352b7a2a920a24265b031b0b20dfc
Fabian <faalgoro@gmail.com>
2022-07-11 10:31:28 -0600

    Add more tests for pow

--- my-project/src/Testing/pow.test.js
+++ my-project/src/Testing/pow.test.js
@@ -3,3 +3,17 @@ const pow = require('../Functions/pow');
 test('pow 4 to equal 16', () => {
   expect(pow(4)).toBe(16);
 });
+
+test('pow 2 to equal 4', () => {
+    expect(pow(2)).toBe(4);
+  });
+
+  test('pow 3 to equal 9', () => {
+    expect(pow(3)).toBe(9);
+  });
+
+  
+  test('pow 1 to equal 1', () => {
+    expect(pow(1)).toBe(1);
+  });
+  
